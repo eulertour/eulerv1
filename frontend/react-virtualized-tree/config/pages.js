3203baf8ec6ba7327b7bd0c286b369316cb9c6dd
Devin Neal <devin@eulertour.com>
2019-03-31 05:53:11 +0000

    remove tree dirs

--- frontend/react-virtualized-tree/config/pages.js
+++
@@ -1,6 +0,0 @@
-const ghpages = require('gh-pages');
-const path = require('path');
-
-ghpages.publish(path.join(__dirname, '../demo/dist'), err => {
-  if (err) throw err;
-});
--- frontend/react-virtualized-tree/src/index.js
+++
@@ -1,8 +0,0 @@
-import Tree from './TreeContainer';
-import * as selectors from './selectors/nodes';
-import renderers from './renderers';
-import * as constants from './contants';
-import FilteringContainer from './FilteringContainer';
-
-export default Tree;
-export {selectors, renderers, constants, FilteringContainer};
