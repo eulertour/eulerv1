57710ebaca908bf4473abe24695f6223275c14fd
Devin <devin@eulertour.com>
2019-04-07 01:15:38 -0700

    add projects page with material ui

--- frontend/src/Projects.js
+++ frontend/src/Projects.js
@@ -1,9 +1,240 @@
 import React from 'react';
+import classNames from 'classnames';
+import { withCookies } from 'react-cookie';
+import { withStyles } from '@material-ui/core/styles';
+import logo from './assets/etourlogo.jpg';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import TextField from '@material-ui/core/TextField';
+import PropTypes from 'prop-types';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+
+const drawerWidth = 260;
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    backgroundColor: theme.palette.grey[100],
+    height: 'calc(100vh)',
+  },
+  appBar: {
+    display: 'flex',
+    justifyContent: 'center',
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    zIndex: theme.zIndex.drawer + 1,
+    minHeight: 70,
+    backgroundColor: theme.palette.common.white,
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: drawerWidth,
+  },
+  menuButton: {
+    padding: 12,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    ...theme.mixins.toolbar,
+    justifyContent: 'space-between',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: 0,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: drawerWidth,
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  search: {
+    position: 'relative',
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: theme.palette.common.black,
+  },
+  inputRoot: {
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+  },
+  toolbar: {
+      display: 'flex',
+      justifyContent: 'space-around',
+      ...theme.mixins.toolbar,
+  },
+  colorBlack: {
+    color: theme.palette.common.black,
+  }
+});
 
 class Projects extends React.Component {
+    state = {
+        drawerOpen: false,
+        sharedProjects: true,
+    }
+
+    handleDrawerToggle = () => {
+        this.setState({drawerOpen: !this.state.drawerOpen});
+    }
+
     render() {
+        const { classes } = this.props;
+        const { drawerOpen, sharedProjects } = this.state;
         return (
-            <div>projects</div>
+            <div className={classes.root}>
+                <AppBar
+                    position="fixed"
+                    className={classes.appBar}
+                >
+                    <Toolbar
+                        className={classes.toolbar}
+                        disableGutters={true}
+                    >
+                        <div className="projects-logo-container">
+                            <div className="logo-container">
+                                <IconButton
+                                    color="default"
+                                    aria-label="Open drawer"
+                                    onClick={this.handleDrawerToggle}
+                                    className={classes.menuButton}
+                                    classes={{
+                                        label: classes.colorBlack,
+                                    }}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <img
+                                    className="banner-logo"
+                                    src={logo}
+                                    alt="EulerTour logo"
+                                />
+                                <div className="banner-text">
+                                    EulerTour
+                                    <sup className="release">&alpha;</sup>
+                                </div>
+                            </div>
+                        </div>
+                       <div className={classes.search}>
+                         <div className={classes.searchIcon}>
+                           <SearchIcon />
+                         </div>
+                         <TextField
+                           variant="outlined"
+                           InputProps={{
+                             placeholder:"Search…",
+                             classes:{
+                               root: classes.inputRoot,
+                               input: classes.inputInput,
+                             }
+                           }}
+                         />
+                       </div>
+                    </Toolbar>
+                </AppBar>
+                <div
+                    className={classNames(classes.content, {
+                        [classes.contentShift]: drawerOpen,
+                    })}
+                >
+                    <div className={classes.drawerHeader} />
+                    {sharedProjects ? "shared projects" : "your projects"}
+                </div>
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={drawerOpen}
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <div className={classes.toolbar} />
+                    <List>
+                      <ListItem
+                        button
+                        key={"yourprojects"}
+                        onClick={() => {this.setState({sharedProjects: false})}}
+                      >
+                        <ListItemIcon><InboxIcon /></ListItemIcon>
+                        <ListItemText variant="h3" primary={"Your Projects"} />
+                      </ListItem>
+                      <ListItem
+                        button
+                        key={"sharedprojects"}
+                        onClick={() => {this.setState({sharedProjects: true})}}
+                      >
+                        <ListItemIcon><InboxIcon /></ListItemIcon>
+                        <ListItemText variant="h3" primary={"Shared Projects"} />
+                      </ListItem>
+                    </List>
+                </Drawer>
+            </div>
         );
     }
 }
+
+Projects.propTypes = {
+  theme: PropTypes.object.isRequired,
+};
+
+export default withCookies(withStyles(styles, { withTheme: true })(Projects));
